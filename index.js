8145a57c6f1325b8883b2f27fde33f58eb8c5dcb
Paulo Salvatore <salvatoregames@gmail.com>
2023-03-16 21:05:29 -0300

    Read by ID implementado

--- index.js
+++ index.js
@@ -10,10 +10,29 @@ app.get("/oi", function (req, res) {
 });
 
 const itens = ["Homem de Ferro", "Mulher Maravilha", "Capitã Marvel"];
+//              0                 1                   2
 
 // Read All [GET] /itens
 app.get("/itens", function (req, res) {
   res.send(itens);
+});
+
+// Read Single [GET] /itens/:id
+app.get("/itens/:id", function (req, res) {
+  // Obtemos o ID que foi enviado pelo parâmetro
+  //  de rota
+  const id = req.params.id;
+
+  // Como o índice da lista começa em 0 e
+  //  queremos que o ID comece em 1,
+  //  precisamos subtrair 1 do ID, para
+  //  garantir que ele vai acessar o item
+  //  correto da lista
+  // A partir do ID, acessamos ele na lista
+  const item = itens[id - 1];
+
+  // Enviamos a resposta com o item obtido
+  res.send(item);
 });
 
 app.listen(3000);
